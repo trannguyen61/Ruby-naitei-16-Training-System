a2b2274801ff4e6387c371899be148d13145c469
Tran Nguyen <trannguyen61st@gmail.com>
2021-08-25 14:42:34 +0700

    #39930/Update progress

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,13 +1,17 @@
 class StatusesController < ApplicationController
-  before_action :logged_in_user
-  before_action :load_status
+  before_action :logged_in_user, :load_status, :load_task_status
+  before_action :correct_user, :correct_start_time, only: %i(update)
 
-  def show
-    if @status.finishable_type == "Subject"
-      @task_statuses = @enrollment.statuses
-                                  .tasks_subject_id @status.finishable_id
+  def show; end
+
+  def update
+    @update_obj = Status.find_by id: params[:status_id]
+    if @update_obj.update update_params
+      handle_finished_all_subjects
+      flash[:success] = t "success_updated"
+      redirect_to @status
     else
-      fail_respond t("data_not_found"), courses_path
+      fail_respond t("data_not_found"), @status
     end
   end
 
@@ -20,5 +24,42 @@ class StatusesController < ApplicationController
     else
       fail_respond t("data_not_found"), courses_path
     end
+  end
+
+  def correct_user
+    redirect_to courses_path unless current_user? @status.user
+  end
+
+  def update_params
+    params.permit Status::UPDATE_ATTRS
+  end
+
+  def load_task_status
+    if @status.finishable_type == "Subject"
+      @task_statuses = @enrollment.statuses
+                                  .tasks_subject_id @status.finishable_id
+      @finished_rate = @status.subject_finished_rate
+    else
+      fail_respond t("data_not_found"), courses_path
+    end
+  end
+
+  def handle_finished_all_subjects
+    subject_statuses = @enrollment.statuses.subjects_ordered
+    has_finished = subject_statuses
+                   .reduce(true){|a, e| a && e.finished?}
+
+    return unless has_finished
+
+    finish_time = @update_obj.updated_at
+    return if @enrollment.update finish_time: finish_time
+
+    fail_respond t("data_not_found"), courses_path
+  end
+
+  def correct_start_time
+    return unless @status.finishable.start_time > Time.now.utc
+
+    fail_respond t("wrong_start_time"), courses_path
   end
 end
